0b7e4443f30853f521e191fe3543132480b581d1
IamXiaRui <iamxiarui@foxmail.com>
2016-03-11 17:56:50 +0800

    插入排序

--- xr_java_algorithm/src/xr_algorithm_sort/InsertSortTest.java
+++ xr_java_algorithm/src/xr_algorithm_sort/InsertSortTest.java
@@ -14,39 +14,39 @@ package xr_algorithm_sort;
 public class InsertSortTest {
 
 	public static void main(String[] args) {
-		int[] a = { 49, 52, 10, 69, 100, 75, 32, 41, 65, 5, 82, 28, 13, 57, 16 };
+		int[] arr = { 49, 52, 10, 69, 100, 75, 32, 41, 65, 5, 82, 28, 13, 57, 16 };
 
 		sop("排序前： ");
-		for (int i = 0; i < a.length; i++) {
-			sop(" " + a[i]);
+		for (int i = 0; i < arr.length; i++) {
+			sop(" " + arr[i]);
 		}
 
-		InsertSort(a);
+		InsertSort(arr);
 
 		sop("\n排序后： ");
-		for (int i = 0; i < a.length; i++) {
-			sop(" " + a[i]);
+		for (int i = 0; i < arr.length; i++) {
+			sop(" " + arr[i]);
 		}
 	}
 
 	// 排序方法
-	public static void InsertSort(int[] arr) {
+	public static void InsertSort(int[] a) {
 		// 初始化一个标志元素
 		int temp = 0;
 
 		// 从表中第二个元素开始
-		for (int i = 1; i < arr.length; i++) {
+		for (int i = 1; i < a.length; i++) {
 			// 排序从第二个元素开始 因为默认第一个元素已经是有序的
-			temp = arr[i];
+			temp = a[i];
 			// j是当前元素的前一个元素 因为每次只需要从前一个开始逆向扫描 之前的已经是有序的
 			int j = i - 1; // 从当前元素的前一个元素开始 与其逆向比较
-			for (; j >= 0 && temp < arr[j]; j--) {
+			for (; j >= 0 && temp < a[j]; j--) {
 				// 如果后面的比前面小 则置换位置 //
 				// 整个循环相当于把大于temp的所有元素整体往后移一个单位 就像当前元素插入一样
-				arr[j + 1] = arr[j];
+				a[j + 1] = a[j];
 			}
 			// 如果不满足 后面元素比前面大 后面保持位置不动 再循环找后面的元素
-			arr[j + 1] = temp;
+			a[j + 1] = temp;
 		}
 	}
 
